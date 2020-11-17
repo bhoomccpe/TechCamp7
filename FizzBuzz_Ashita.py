577e20a2b618359f0498237addb2df50f02c98d4
Kidleel <ashita11479@gmail.com>
2020-11-17 17:37:38 +0700

    fizzbuzz_Ashita

---
+++ FizzBuzz_Ashita.py
@@ -0,0 +1,10 @@
+x = int(input("add number for fizzbuzz : "))
+for i in range(1,x+1):
+    if i%3 == 0 and i%5 == 0:
+        print("fizzbuzz")
+    elif i%3 == 0:
+        print("fizz")
+    elif i%5 == 0:
+        print("buzz")
+    else:
+        print(i)
